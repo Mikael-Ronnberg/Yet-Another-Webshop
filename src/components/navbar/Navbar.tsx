898dc762e45358a8896f5f1fdb66587166b54122
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-18 17:57:08 +0200

    added more styling and logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,29 +4,31 @@ export const Navbar = () => {
   return (
     <>
       <header className="navbar">
-        <span className="blue_span"></span>
+        <div className="blue_span"></div>
         <span className="green_span">
           {" "}
           PIECES FROM OUR PAST, TO SAVE OUR FUTURE 🌎
         </span>
-        <div className=""></div>
-        <nav>
-          <ul>
-            <li>NEW IN</li>
-            <li>WOMEN</li>
-            <li>MEN</li>
-            <li>EVERYONE</li>
-            <li>BRANDS</li>
-            <li>SPORTS</li>
-            <li>DENIM</li>
-            <li>SUMMER</li>
-            <li>SALE & DEALS</li>
-            <li>ABOCE & BEYOND</li>
-            <li>SWIPER</li>
-          </ul>
-        </nav>
-        <div>Logo</div>
-        <div className="login"></div>
+        <div className="white_span">
+          <div className="search_nav"></div>
+          <nav>
+            <ul>
+              <li>NEW IN</li>
+              <li>WOMEN</li>
+              <li>MEN</li>
+              <li>EVERYONE</li>
+              <li>BRANDS</li>
+              <li>SPORTS</li>
+              <li>DENIM</li>
+              <li>SUMMER</li>
+              <li>SALE & DEALS</li>
+              <li>ABOCE & BEYOND</li>
+              <li>SWIPER</li>
+            </ul>
+          </nav>
+          <div className="logo">Logo</div>
+          <div className="login"></div>
+        </div>
       </header>
     </>
   );
