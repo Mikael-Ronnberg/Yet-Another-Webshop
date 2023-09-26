bbc2b800f7b2ded35ae1954a900d05130bc7a825
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-26 20:34:39 +0200

    adding content to productItem

--- src/components/productitem/ProductItem.tsx
+++ src/components/productitem/ProductItem.tsx
@@ -1,9 +1,16 @@
 import { IProductProps } from "../../models/IProductProps";
 
 export const ProductItem = (product: IProductProps) => {
+  const { productTitel, productDescription, price, expireDate } = product;
+
   return (
     <>
-      {/* <div className="productContainer">{product.map((p) => {})}</div> */}
+      <div className="productContainer">
+        <h3>{productTitel}</h3>
+        <p>{productDescription}</p>
+        <h5>{price}</h5>
+        <p>{expireDate}</p>
+      </div>
     </>
   );
 };
