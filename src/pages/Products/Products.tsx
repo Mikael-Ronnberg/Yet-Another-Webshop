aa704ece888905f4b06c29e6f26d9562a92a748d
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-26 20:41:02 +0200

    Tried with wome mockdata

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,9 +1,42 @@
 import { Navbar } from "../../components/navbar/Navbar";
+import { ProductItem } from "../../components/productitem/ProductItem";
+import { IProductProps } from "../../models/IProductProps";
 
 export const Products = () => {
+  const products: IProductProps[] = [
+    {
+      productTitel: "Fiskmås",
+      productDescription: "Gott med fiskmås",
+      price: 2334,
+      expireDate: 21062023,
+      quantity: 1,
+    },
+    {
+      productTitel: "Fiskmås",
+      productDescription: "Gott med fiskmås",
+      price: 2334,
+      expireDate: 21062023,
+      quantity: 1,
+    },
+    {
+      productTitel: "Fiskmås",
+      productDescription: "Gott med fiskmås",
+      price: 2334,
+      expireDate: 21062023,
+      quantity: 1,
+    },
+  ];
+
   return (
     <>
       <Navbar></Navbar>
+      <main className="mainContainer">
+        <div className="mainContainer__products">
+          {/* {products.map(p, i) => {
+            <ProductItem key={i} product={p}></ProductItem>;
+          })} */}
+        </div>
+      </main>
     </>
   );
 };
