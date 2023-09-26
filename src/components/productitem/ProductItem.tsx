0c5406d4415a84af2870a342516d5f11e983dd64
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-26 20:25:41 +0200

    Build: adding product page and content

---
+++ src/components/productitem/ProductItem.tsx
@@ -0,0 +1,9 @@
+import { IProductProps } from "../../models/IProductProps";
+
+export const ProductItem = (product: IProductProps) => {
+  return (
+    <>
+      {/* <div className="productContainer">{product.map((p) => {})}</div> */}
+    </>
+  );
+};
---
+++ src/models/IProductProps.ts
@@ -0,0 +1,7 @@
+export interface IProductProps {
+  productTitel: string;
+  productDescription: string;
+  price: number;
+  expireDate: number;
+  quantity: number;
+}
