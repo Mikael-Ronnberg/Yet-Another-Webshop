bc6b8edcb02ffc62674e3925dd13f86a5b29dc63
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-11-15 22:18:20 +0100

    changed rel-path for deploy

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -5,16 +5,13 @@ import { products } from "../../mocks/products";
 import { Footer } from "../../components/footer/Footer";
 
 export const Products = () => {
-  // const shoppingCart = useContext(CartContext);
-  // const dispatch = useContext(CartDispatchContext);
-
   return (
     <>
       <Navbar></Navbar>
       <Hero
         heading="Green and Beatiful Plants"
         description="Dr. Greenthumb would be jealouse!"
-        image="/src/assets/hero/hero_image.png"
+        image="/dist/hero/hero_image.png"
         btnLabel="View All Categories"
         btnLink="/categories"
       ></Hero>
